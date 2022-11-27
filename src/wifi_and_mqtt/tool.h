e13e5bb6b483d2c2709d69907a44d3a518abc9dc
Adam Spontarelli <adam@vector-space.org>
2022-11-27 16:30:58 -0500

    working prototype that includes mqtt and wifi

---
+++ src/wifi_and_mqtt/tool.h
@@ -0,0 +1,44 @@
+#include "EspMQTTClient.h"
+
+class Tool{
+public:
+    int PIN;
+    String name;
+    bool tool_state = false;
+    bool old_tool_state = false;
+    String message;
+
+    unsigned long currentTime;
+    unsigned long cloopTime = millis();
+
+    void set_pin(int pin){
+        PIN = pin;
+        pinMode(pin, INPUT_PULLUP);
+    }
+
+    void turn_on(EspMQTTClient &clt){
+        tool_state = true;
+        message = name + ", " + tool_state;
+        //message = tool_name;
+        if (tool_state != old_tool_state){
+            Serial.println("tool state has changed");
+            clt.publish("tools/dust_collection", message);
+            old_tool_state = tool_state;
+        }
+    }
+    
+    void turn_off(EspMQTTClient &clt){
+        tool_state = false;
+        message = name + ", " + tool_state;
+        if (tool_state != old_tool_state){
+            Serial.println("tool state has changed");
+            clt.publish("tools/dust_collection", message);
+            old_tool_state = tool_state;
+        }
+    }
+
+    bool get_state(){
+        Serial.println(tool_state);
+        return tool_state;
+    }
+};
